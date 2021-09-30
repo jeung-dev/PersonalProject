b13a8670d20cbff4e1c56cdcd15844f317d4574c
JeungEun <anjeundeun@gmail.com>
2021-09-30 23:35:47 +0900

    CustomPupupView의 Content영역: View 그리고 ImageView를 배열로 넣어서 컨텐츠를 띄우도록 수정

--- Personal/Scenes/Sub/SubViewController.swift
+++ Personal/Scenes/Sub/SubViewController.swift
@@ -79,6 +79,46 @@ class SubViewController: BaseViewController, SubDisplayLogic, SkeletonDisplayabl
 extension SubViewController {
     
     func getContentsViewInPopup() {
+        /**
+         # Version 1.
+         이미지뷰만을 띄움
+         
+         
+         //컨텐츠뷰(in popoupview)
+         guard let contentsInPopup = ContentsInPopup.loadViewFromNib() else { return }
+         contentsInPopup.frame.size.width = self.view.frame.width
+         contentsInPopup.setImageView()  //스켈레톤을 보이게 하기 위해 꼭 이미지를 띄워야 함
+         self.view.addSubview(contentsInPopup)
+         
+         //이미지가 로드되는 동안 흰 화면이 보이는 문제로
+         //스켈레톤 화면을 띄워서 해결함.
+         //스켈레톤 화면 위에 로딩 인디케이터도 띄움
+         //인디케이터와 스켈레톤 Start
+         showSkeleton()
+         setLoadingIndicator()
+         self.startLoadingIndicator()
+         
+         //이미지를 url에서 불러옴
+         //이미지가 없으면 기본이미지를 보여줌
+         let imageView = UIImageView()
+         let urlString = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8uyx3Z85xVn1ie0BCR3aY9U2gs4BbK1kAXg&usqp=CAU"
+         imageView.loadImageWithURLString(urlString, cacheKey: urlString, filter: nil)
+         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+             contentsInPopup.setImage(imageView.image ?? UIImage.getPlaceholder(size: CGSize(width: self.view.frame.width, height: 300)))
+             
+             //인디케이터와 스켈레톤 Stop
+             self.stopLoadingIndicator()
+             self.hideSkeleton()
+             
+         }
+         
+         */
+        
+        
+        /**
+         # Version 2
+         여러 컨텐츠를 여기서 만들어서 팝업뷰에 넣음
+         */
         
         
         //컨텐츠뷰(in popoupview)
@@ -86,6 +126,7 @@ extension SubViewController {
         contentsInPopup.frame.size.width = self.view.frame.width
         contentsInPopup.setImageView()  //스켈레톤을 보이게 하기 위해 꼭 이미지를 띄워야 함
         self.view.addSubview(contentsInPopup)
+        
         
         //이미지가 로드되는 동안 흰 화면이 보이는 문제로
         //스켈레톤 화면을 띄워서 해결함.
@@ -95,18 +136,33 @@ extension SubViewController {
         setLoadingIndicator()
         self.startLoadingIndicator()
         
-        //이미지를 url에서 불러옴
-        //이미지가 없으면 기본이미지를 보여줌
-        let imageView = UIImageView()
+        
+        //여러 컨텐츠를 만듦
+        
+        //View
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 400))
+        view.backgroundColor = .yellow
+        
+        //ImageView
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 1000))
         let urlString = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8uyx3Z85xVn1ie0BCR3aY9U2gs4BbK1kAXg&usqp=CAU"
         imageView.loadImageWithURLString(urlString, cacheKey: urlString, filter: nil)
+        
+        //Label
+        let label = UILabel()
+        label.text = "신데렐라는 어려서 부모님을 잃고요, 계모와 언니들에게 괴롭힘을 당했더래요~"
+        
+        
+        //이미지가 로드 된 뒤에 화면을 띄우기 위해서 DispatchQueue를 이용함
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            contentsInPopup.setImage(imageView.image ?? UIImage.getPlaceholder(size: CGSize(width: self.view.frame.width, height: 300)))
+            contentsInPopup.setItems([view, imageView])
             
             //인디케이터와 스켈레톤 Stop
             self.stopLoadingIndicator()
             self.hideSkeleton()
             
         }
+        
+        
     }
 }
--- Personal/Scenes/Views/ContentsInPopup.swift
+++ Personal/Scenes/Views/ContentsInPopup.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ContentsInPopup: UIView {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentsView: UIView!
+    @IBOutlet weak var labelBottomConstraint: NSLayoutConstraint!
     
     let imageView: UIImageView = {
         let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
@@ -49,8 +51,89 @@ class ContentsInPopup: UIView {
         
     }
     
+    func removeAllSubViews(){
+            for subview in self.contentsView.subviews {
+                subview.removeFromSuperview()
+            }
+        }
+
     func setItems(_ views: [UIView]) {
+        
+        //뷰들을 추가하기 전에 이전 뷰를 모두 지우자
+        self.removeAllSubViews()
+        
         //MARK: TODO 아이템 순서대로 띄우는 코드
+        
+        var preView: UIView?    //constraint top을 잡기 위한 이전 뷰
+        let viewsCount = views.count    //마지막 뷰를 알기 위함
+        var count = 0   //마지막 뷰를 알기 위함
+        
+        for view in views {
+            
+            //마지막 뷰를 알아채기 위한 카운트
+            count += 1
+            
+            //크기받아오기
+            let newView = self.getResizeByKeepingAspectRatioView(view, toWidth: UIScreen.main.bounds.width)
+            
+            //Add
+            self.contentsView.addSubview(newView)   //추가하지 않으면 에러남 --> 조상이 없다는 문구가 있어서 contentView에 추가했더니 에러가 안남. 추후에 없애면 constraint가 이상해짐.
+            self.contentsView.addSubview(view)
+            
+            //Constraint 조절
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+            //만약 이전 뷰가 없으면 첫번째 뷰임.
+            if preView == nil {
+                NSLayoutConstraint.activate([
+                    view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor),
+                    view.topAnchor.constraint(equalTo: view.superview!.topAnchor),
+                    view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor),
+                    view.heightAnchor.constraint(equalTo: newView.heightAnchor)
+                ])
+                preView = view
+            } else {
+                //마지막 뷰일 때, bottomAnchor 정하기
+                if count != viewsCount {
+                    NSLayoutConstraint.activate([
+                        view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor),
+                        view.topAnchor.constraint(equalTo: preView!.bottomAnchor),
+                        view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor),
+                        view.heightAnchor.constraint(equalTo: newView.heightAnchor)
+                    ])
+                } else {
+                    NSLayoutConstraint.activate([
+                        view.leadingAnchor.constraint(equalTo: view.superview!.leadingAnchor),
+                        view.topAnchor.constraint(equalTo: preView!.bottomAnchor),
+                        view.trailingAnchor.constraint(equalTo: view.superview!.trailingAnchor),
+                        view.bottomAnchor.constraint(equalTo: view.superview!.bottomAnchor),
+                        view.heightAnchor.constraint(equalTo: newView.heightAnchor)
+                    ])
+                }
+                preView = view
+            }
+        }
+        scrollView.updateContentView()  //scrollView 높이 재정의(뷰들의 높이에 맞춤)
+        self.imageView.removeFromSuperview()    //스켈레톤을 띄우던 이미지뷰 삭제
+    }
+    
+    func getResizeByKeepingAspectRatioView(_ source: UIView, toWidth: CGFloat) -> UIView {
+        let oldWidth = source.frame.size.width
+        let scaleFactor = toWidth / oldWidth
+        let newHeight = source.frame.size.height * scaleFactor
+        let newWidth = oldWidth * scaleFactor;
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        
+        let rect = CGRect(origin: .zero, size: newSize)
+        let view = UIView(frame: rect)
+        return view
     }
     
 }
+
+
+extension UIScrollView {
+    func updateContentView() {
+         contentSize.height = subviews.sorted(by: { $0.frame.maxY < $1.frame.maxY }).last?.frame.maxY ?? contentSize.height
+     }
+ }
